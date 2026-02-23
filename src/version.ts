53ba044940f2d7ec6141b000774870e555b55ca9
Lars Gregori <138015+choas@users.noreply.github.com>
2026-02-23 23:43:13 +0100

    Sync with upstream v1.1.1 (2026-02-23)

--- src/version.ts
+++ src/version.ts
@@ -11,7 +11,7 @@
 declare const __BUILD_TIME__: string | undefined;
 
 /** Base TronOS version number */
-const BASE_VERSION = '1.1.0';
+const BASE_VERSION = '1.1.1';
 
 /**
  * Check if running in development mode.
